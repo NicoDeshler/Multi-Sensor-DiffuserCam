e297b41450f9d52a6f40da0a6971d9a7726da92d
NicoDeshler <nico.deshler@gmail.com>
2018-05-17 14:35:23 -0700

    Update MATLABStandaloneApplication.m

--- MATLABStandaloneApplication.m
+++ MATLABStandaloneApplication.m
@@ -1,8 +1,4 @@
-function [ r ] = MATLABStandaloneApplication( args )
-
-if ~exist('args', 'var')
-    args = [];
-end
+function [r] = MATLABStandaloneApplication()
 
 % Initialize the OpticStudio connection
 TheApplication = InitConnection();
@@ -11,7 +7,7 @@ if isempty(TheApplication)
     r = [];
 else
     try
-        r = BeginApplication(TheApplication, args);
+        r = BeginApplication(TheApplication);
         CleanupConnection(TheApplication);
     catch err
         CleanupConnection(TheApplication);
@@ -34,8 +30,7 @@ import System.Reflection.*;
 % NET.addAssembly(p);
 
 % This uses a hard-coded path to OpticStudio
-NET.addAssembly('C:\Users\Deshler\Documents\Zemax\ZOS-API\Libraries\ZOSAPI_NetHelper.dll');
-
+NET.addAssembly('C:\Users\Deshler\Documents\Zemax\ZOS-API\Libraries\ZOSAPI_NetHelper.dll'); 
 success = ZOSAPI_NetHelper.ZOSAPI_Initializer.Initialize();
 % Note -- uncomment the following line to use a custom initialization path
 % success = ZOSAPI_NetHelper.ZOSAPI_Initializer.Initialize('C:\Program Files\OpticStudio\');
@@ -68,128 +63,142 @@ if ~app.IsValidLicenseForAPI
     HandleError('License check failed!');
     app = [];
 end
-
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%   USER INPUT BEGINS BELOW    %%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+%% Boilerplate Code
 
-function [r] = BeginApplication(TheApplication, args)
-% BEGIN APPLICATION Run a session of Zemax Optic Studio. 
-% User inputs include 
-% The lens file directory and save folder directory.
+function [r] = BeginApplication(TheApplication)
+% BEGINAPPLICATION Contains all user input variables and handles
+% input errors. Executes specified analyses and handles data saving.
 
 import ZOSAPI.*;
-
-% Initiate Primary System
 TheSystem = TheApplication.PrimarySystem;
-
-% File/Folder locations
-LensFile = 'C:\Users\Deshler\Documents\Zemax\Samples\Diffuser Setups\MatLab Diffuser\Basic DiffuserCam.zmx';
-SaveFolder = 'PUT YOUR SAVE FOLDER HERE';
-
-% Load in the lens file 
-TheSystem.LoadFile(LensFile, false);
-
-% Run specified analyses
-Analyze(TheApplication);
-
-disp("Analysis complete. Data files saved.")
-r = [];
-
-end
-
-
-function Analyze(TheApplication)
-% ANALYZE Runs analyses specified by the user in the function body.
-
-% Session Variables
 TheAnalyses = TheApplication.PrimarySystem.Analyses;
 TheLDE = TheApplication.PrimarySystem.LDE; 
 
+% Load in lensfile files from provided locations
+lensFile = 'C:\Users\Deshler\Documents\Zemax\Samples\DiffuserCam Setups\Setups with MatLab Diffuser\Basic DiffuserCam.zmx'; %where the lens file is located
+saveFolder = 'C:\Users\Deshler\Documents\Zemax\ZOS-API Projects\MATLABStandaloneApplication\DiffuserCam Analyses'; %where analysis results will be stored
+if ~exist(saveFolder, 'dir')
+    HandleError('Provided save folder does not exist.');
+end
+TheSystem.LoadFile(lensFile, false)
+
+% The list of user-defined analyses functions
+a1 = @PhysicalOpticsProp;
+a2 = @SpotDiagram;
+a3 = @SampleAnalysis3;
+a4 = @SampleAnalysis4;
+
+%Specify which analyses to run here e.g. {a1, a2, ...}
+toRun = {a1}; 
+
+% Run the specified analyses and save stack video if 
+% visualize is true
+visualize = false;
+for aN = 1 : size(toRun)
+    [data, filename] = toRun{aN}(TheAnalyses, TheLDE);
+    saveResults(data, filename, saveFolder);    
+    if visualize
+        avi = makeVideo(data, filename);
+        saveResults(avi, avi.Filename, saveFolder);
+    end
+end
+
+r = [];
+end
+
+
+function [zStack, filename] = PhysicalOpticsProp(TheAnalyses, TheLDE)
 % Surfaces
-Source = TheLDE.GetSurfaceAt(0);
-ImageSpacing = TheLDE.GetSurfaceAt(2);
+s0 = TheLDE.GetSurfaceAt(0);
+s2 = TheLDE.GetSurfaceAt(2);
+
+% Intantiate the POP Analysis object
+pop = TheAnalyses.New_Analysis_SettingsFirst(ZOSAPI.Analysis.AnalysisIDM.PhysicalOpticsPropagation);
+
+% Configure POP Settings | NOTE: Make sure you adjust your POP settings
+% in your lens file using the OpticStudio suite and 'save' the settings.
+% Few of the POP settings are easily adjustable in the standalone connection. 
+pop_settings = pop.GetSettings();
+
+
+maxDist = 20; %millimeters 
+minDist = 0; %millimeters
+n = 40; 
 i = 1;
+zStack = zeros(512,512,n); %Specify POP dimensions
 
 % Image Plane Depth: linspace(max, min, n)
-for z = linspace(1,0.1,20) 
+for z = linspace(maxDist, minDist, n) 
     
     % Set image plane depth
-    ImageSpacing.Thickness = z;
-    disp(z)
-    
-    % Physical Optics Propogation Analysis
-    pop = TheAnalyses.New_Analysis_SettingsFirst(ZOSAPI.Analysis.AnalysisIDM.PhysicalOpticsPropagation);
-
-    % Configure POP Settings | NOTE: Make sure you adjust your POP settings
-    % in your lens file using the OpticStudio suite and 'save' the settings.
-    % Few of the POP settings are easily adjustable in the standalone connection. 
-    pop_settings = pop.GetSettings();
+    s2.Thickness = z;
     
     % Run the POP Analysis
     pop.ApplyAndWaitForCompletion();
     pop_results = pop.GetResults();
     pop_grid = pop_results.GetDataGrid(0);
     pop_data = pop_grid.Values.double;
-                
-    
-    % Spot Diagram Analysis
-    spot = TheAnalyses.New_StandardSpot();
-
-    % Configure Spot Settings
-    spot_settings = spot.GetSettings();
-    spot_settings.Pattern = ZOSAPI.Analysis.Settings.Spot.Patterns.Square;
-
-    % Run the Spot Analysis
-    spot.ApplyAndWaitForCompletion();
-    spot_results = spot.GetResults();
-    spot_data = spot_results.SpotData;
-    
-    
-    WriteData(pop_data, z);
-    
-    imagesc(pop_data)
-    Mov(i) = getframe(gcf);
+        
+    % Push POP data onto Z stack
+    zStack(:,:,i) = pop_data;
     i = i + 1;
 end
-
-WriteMov(Mov);
-
-end
-
-function WriteMov(figures)
-v = VideoWriter('CloseCaustics.avi', 'Uncompressed AVI');
-v.FrameRate = 15;
-open(v)
-writeVideo(v, figures)
-close(v)
+filename = sprintf('POPData (Max_%dmm Min_%dmm N_%d)', maxDist, minDist, n);
 end
 
 
-% Write data to SaveDir as .mat file. Filename contains source depth info.
-function WriteData(pop_data, z)
+function [data, filename] = SpotDiagram(TheAnalyses, TheLDE)
+% Surfaces
 
+% Spot Diagram Analysis
+spot = TheAnalyses.New_StandardSpot();
+
+% Configure Spot Settings
+spot_settings = spot.GetSettings();
+spot_settings.Pattern = ZOSAPI.Analysis.Settings.Spot.Patterns.Square;
+
+% Run the Spot Analysis
+spot.ApplyAndWaitForCompletion();
+spot_results = spot.GetResults();
+spot_data = spot_results.SpotData;
+
+data = spot_data;
+filename = 'SpotData';    
+end
+
+
+function saveResults(data, filename, saveFolder)
+% SAVERESULTS saves data collected in an analysis to the  
 timestamp = datestr(now,'mm-dd-yy');
-% AnalysisDir = strcat('YOUR SAVE FOLDER PATH', timestamp, '\');
-AnalysisDir = strcat('C:\Users\Deshler\Documents\MATLAB\DiffuserCam Simulations\Zemax Analysis_', timestamp, '\');
-if ~exist(AnalysisDir)
-    mkdir(AnalysisDir)
+subdir = strcat(saveFolder, timestamp,'\'); %ADD ANOTHER '\' BEFORE THE TIMESTAMP
+if ~exist(subdir, 'dir')
+    mkdir(subdir)
+end
+save(strcat(subdir, filename), 'data');
+LogMessage(strcat(filename, 'has been saved.'))
 end
 
-% Save raw data
-filename = strcat(AnalysisDir, sprintf('pop_data @ %d microns', int64(z*1000)));
-% save(filename, 'pop_data')
 
-% Save color-scale image
-%colorIm = figure;
-%imagesc(pop_data);
-%saveas(colorIm, filename, 'png');
-%close all
-
-disp("POP data written to folder.")
-
+function v = makeVideo(data, filename)
+frameNum = size(data, 3);
+video = zeros(1,frameNum); 
+for i = 1 : frameNum
+    frame = imagesc(data(:,:,i));
+    video(i) = frame;
+end    
+    
+vidName = strcat(filename,'Video');
+v = VideoWriter(vidName);
+v.FrameRate = 10;
+open(v)
+try writeVideo(v, video)
+catch writeErr
+    HandleError(writeErr)
+end
+close(v)
 end
 
 
@@ -206,10 +215,7 @@ end
 
 function  CleanupConnection(TheApplication)
 % Note - this will close down the connection.
-
 % If you want to keep the application open, you should skip this step
 % and store the instance somewhere instead.
 TheApplication.CloseApplication();
 end
-
-
